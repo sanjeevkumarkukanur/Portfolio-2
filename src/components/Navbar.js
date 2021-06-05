eebf328a23145c2059315b790e1b77570796d26f
Sanjeevkumar <sanjeev.kukanur@gmail.com>
2021-06-05 16:09:05 +0530

    portfolio

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,50 +1,63 @@
-import React, { useState} from 'react'
-import { DiBlackberry } from 'react-icons/di'
-import { Link } from 'react-router-dom';
-import { FaTimes, FaBars} from 'react-icons/fa'
-import './Navbar.css';
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import { DiCisco} from 'react-icons/di'
+import { FaBars, FaTimes } from 'react-icons/fa'
+import './Navbar.css'
+import {IconContext} from 'react-icons/lib'
 
 
 function Navbar() {
-
-    const [click ,setClick] = useState(false);
-
-
-    const handlerClick = () => setClick(!click)
-    const closeMobileMenu = () => setClick(false)
+  const [click, setClick]= useState(false);
 
 
+  const handlerClick = ()=> setClick(!click);
+  const closeMobileMenu = () => setClick(false)
+
+  const showButton = () => {
+    if(window.innerWidth <= 960){
+      setClick(false)
+    }else{
+      setClick(true)
+    }
+  }
+  useEffect(()=>{
+    showButton()
+  },[]);
+
+  window.addEventListener('resize',showButton);
 
     return (
         <>
-         <div className="navbar">
-             <div className="navbar-container container">
-                 <Link to='/' className="nav-logo">
-                     <DiBlackberry className='navbar-icon' />Sandy
-                 </Link>
-                 <div className="menu-icon" onClick={handlerClick}>
-                     {click ? <FaTimes /> : <FaBars />}
-                 </div>
-                 <ul className={click ? 'nav-menu active':'nav-menu'}>
-                     <li className="nav-items">
-                         <Link to="/" onClick={closeMobileMenu} className='nav-link'>About</Link>
-                     </li>
-                     <li className='nav-items'>
-                         <Link to="/education" onClick={closeMobileMenu} className='nav-link'>Education</Link>
-                     </li>
-                     <li className='nav-items'>
-                         <Link to="/may-skills" onClick={closeMobileMenu} className='nav-link'>My Skills</Link>
-                     </li>
-                     <li className='nav-items'>
-                         <Link to="/contect-us" onClick={closeMobileMenu} className='nav-link'>Contect Us</Link>
-                     </li>
-                     <li className='nav-items'>
-                         <Link to="/more" onClick={closeMobileMenu} className='nav-link'>More</Link>
-                     </li>
-                 </ul>
+        <IconContext.Provider value = {{color: '#fff'}}>
+        <div className="navbar">
+           <div className="navbar-container container">
+             <Link to="/" className="navbar-logo" onClick={handlerClick}>
+               <DiCisco className="navbar-icon" />
+               Sandy
+             </Link>
+             <div  className="menu-icon" onClick={handlerClick}>
+             {click ? <FaTimes /> : <FaBars /> }
              </div>
-
-             </div>   
+             <ul className={click ? 'nav-menu active': 'nav-menu'}>
+               <li className="nav-item">
+               <Link to="/" onClick={closeMobileMenu} className="nav-links">About</Link>
+               </li>
+               <li className="nav-item">
+               <Link to="/education" onClick={closeMobileMenu} className="nav-links">Education</Link>
+               </li>
+               <li className="nav-item">
+               <Link to="/my-skills" onClick={closeMobileMenu} className="nav-links">My Skills</Link>
+               </li>
+               <li className="nav-item">
+               <Link to="/contack-us" onClick={closeMobileMenu} className="nav-links">Contact Us</Link>
+               </li>
+               <li className="nav-item">
+               <Link to='/more' onClick={closeMobileMenu} className='nav-links'>More</Link>
+               </li>
+            </ul> 
+           </div>
+          </div>  
+          </IconContext.Provider>
         </>
     )
 }
