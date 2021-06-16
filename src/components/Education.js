b023f8715649848c198a2ab9d1632730f4c13938
Sanjeevkumar <sanjeev.kukanur@gmail.com>
2021-06-16 18:50:10 +0530

    edu

--- src/components/Education.js
+++ src/components/Education.js
@@ -62,6 +62,54 @@ function Education() {
                                 <button className='btn-sec'>Choose Plan</button>
                             </div>
                         </Link>
+                        <Link to='/sign-up' className="Education__container-card">
+                            <div className="Education__container-cardInfo">
+                                <div className="icon">
+                                    <FaFire />
+                                </div>
+                                <h3>Starter</h3>
+                                <h4>$90.00</h4>
+                                <p>per month</p>
+                                <ul className="Education__container-features">
+                                <li>100 Transactions</li>
+                                <li>2% Cash Back</li>
+                                <li>$10,000 Limit</li>    
+                                </ul> 
+                                <button className='btn-sec'>Choose Plan</button>
+                            </div>
+                        </Link>
+                        <Link to='/sign-up' className="Education__container-card">
+                            <div className="Education__container-cardInfo">
+                                <div className="icon">
+                                    <BsXDiamondFill />
+                                </div>
+                                <h3>Gold</h3>
+                                <h4>$29.00</h4>
+                                <p>per month</p>
+                                <ul className="Education__container-features">
+                                <li>1000 Transactions</li>
+                                <li>3.5% Cash Back</li>
+                                <li>$100,000 Limit</li>    
+                                </ul> 
+                                <button className='btn-sec'>Choose Plan</button>
+                            </div>
+                        </Link>
+                        <Link to='/sign-up' className="Education__container-card">
+                            <div className="Education__container-cardInfo">
+                                <div className="icon">
+                                    <GiCrystalize />
+                                </div>
+                                <h3>Diamond</h3>
+                                <h4>$69.00</h4>
+                                <p>per month</p>
+                                <ul className="Education__container-features">
+                                <li>Unlimited Transactions</li>
+                                <li>5% Cash Back</li>
+                                <li>Unlimited Spinding</li>    
+                                </ul> 
+                                <button className='btn-sec'>Choose Plan</button>
+                            </div>
+                        </Link>x`
                     </div>
                 </div>
             </div>
